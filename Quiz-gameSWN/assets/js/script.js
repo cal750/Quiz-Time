af86d3eb28d2c54d57f928da5c8b8475f55448d8
Seth Nightingale <sethnightingale10@gmail.com>
2021-06-28 16:18:05 +0800

    adding initial variables
    Added some placeholder questions

--- Quiz-gameSWN/assets/js/script.js
+++ Quiz-gameSWN/assets/js/script.js
@@ -1,18 +1,33 @@
-var timeLeft = 30;
-var pos = 0;
-var correct = 0;
-var test, test_status, question, choice, choices, chA, chB, chC;
-var startButton = document.getElementById("startButton");
-var timeleft = 30;
-
-function setTime() {
-    timeLeft--;
-    
-}
-
-
-
-
-
-startButton.addEventListener("click", setTime())
-
+// pos is position of where the user in the test or which question they're up to
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
+var questions = [
+  {
+      question: "What is 36 + 42",
+      a: "64",
+      b: "78",
+      c: "76",
+      answer: "B"
+    },
+  {
+      question: "What is 7 x 4?",
+      a: "21",
+      b: "27",
+      c: "28",
+      answer: "C"
+    },
+  {
+      question: "What is 16 / 4?",
+      a: "4",
+      b: "6",
+      c: "3",
+      answer: "A"
+    },
+  {
+      question: "What is 8 x 12?",
+      a: "88",
+      b: "112",
+      c: "96",
+      answer: "C"
+    }
+  ];
