db6043e209f2c9e7c6d44a64141aecfdcf14060e
Seth Nightingale <sethnightingale10@gmail.com>
2021-06-28 16:21:03 +0800

    This broke it
    I was adding the code and getting ready to edit but it completely broke my flex boxes.

--- Quiz-gameSWN/assets/js/script.js
+++ Quiz-gameSWN/assets/js/script.js
@@ -31,3 +31,42 @@ var questions = [
       answer: "C"
     }
   ];
+
+function get(x){
+    return document.getElementById(x);
+  }
+  function renderQuestion(){
+    test = get("test");
+    if(pos >= questions.length){
+      test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+      get("test_status").innerHTML = "Test completed";
+      pos = 0;
+      correct = 0;
+      return false;
+    }
+    get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+    
+    question = questions[pos].question;
+    chA = questions[pos].a;
+    chB = questions[pos].b;
+    chC = questions[pos].c;
+    test.innerHTML = "<h3>"+question+"</h3>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+  }
+  function checkAnswer(){
+    choices = document.getElementsByName("choices");
+    for(var i=0; i<choices.length; i++){
+      if(choices[i].checked){
+        choice = choices[i].value;
+      }
+    }
+    if(choice == questions[pos].answer){
+      correct++;
+    }
+    pos++;
+    renderQuestion();
+  }
+  window.addEventListener("load", renderQuestion);
